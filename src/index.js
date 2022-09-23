d1b73f403394d06664a06803ba49b95947d968a2
aimeepham14 <aimee.pham14@gmail.com>
2022-09-22 19:32:25 -0700

    finished deliverable

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyList from './MyList';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const toDos = ["Do laundry", "Recycle Cans", "Get gas"]
+
+ReactDOM.render(
+  <MyList theList={toDos} />,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
